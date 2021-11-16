f931aa0d622e59a08165ba7529551851fb8d1969
Nikola <woleet@outlook.com>
2021-11-16 04:21:40 +0100

    Added authentication system (create and destroy sessions)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user and user.authenticate(params[:session][:password])
+      # good scenario
+      session[:user_id] = user.id
+      flash[:success] = "Welcome to Shoutbox!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Unexpected error has occured"
+      render 'new'
+    end
+  end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have been succesfully logged out"
+    redirect_to login_path
   end
 
 end
